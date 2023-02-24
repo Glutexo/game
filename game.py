2d0ec2a7cb43d49f89c40af4927b57c57d3bf0e0
Glutexo <glutexo@icloud.com>
2023-02-24 14:00:14 +0100

    Make State namedtuple instead of dataclass

--- game.py
+++ game.py
@@ -1,5 +1,4 @@
-from dataclasses import dataclass
-from dataclasses import replace
+from collections import namedtuple
 from enum import auto
 from enum import Enum
 
@@ -8,11 +7,7 @@ class Commands(Enum):
     EXIT = auto()
 
 
-@dataclass
-class State:
-    player_count: ...
-    active_player: ...
-
+class State(namedtuple("State", ["player_count", "active_player"])):
     @classmethod
     def init(cls):
         return cls(player_count=2, active_player=0)
@@ -21,7 +16,7 @@ class State:
         next_player = self.active_player + 1
         if next_player == self.player_count:
             next_player = 0
-        return replace(self, active_player=next_player)
+        return State(**{**self._asdict(), "active_player": next_player})
 
 
 def loop(state, callback):
