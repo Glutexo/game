f74c0440e6589756dd8a579488990a61fcad6a05
Glutexo <glutexo@icloud.com>
2023-02-24 13:59:35 +0100

    Don’t use replace in tests

--- tests/state/test_next_player.py
+++ tests/state/test_next_player.py
@@ -1,17 +1,13 @@
-from dataclasses import replace
-
 from game import State
 
 
 def test_0_to_1():
-    state = State.init()
-    state = replace(state, active_player=0)
+    state = State(player_count=2, active_player=0)
     state = state.next_player()
     assert state.active_player == 1
 
 
 def test_1_to_0():
-    state = State.init()
-    state = replace(state, active_player=1)
+    state = State(player_count=2, active_player=1)
     state = state.next_player()
     assert state.active_player == 0
