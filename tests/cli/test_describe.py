a5450c77e2896d93838d97405d1b7c55e86003e6
Glutexo <glutexo@icloud.com>
2023-02-24 13:50:31 +0100

    Don’t use replace in describe tests

--- tests/cli/test_describe.py
+++ tests/cli/test_describe.py
@@ -1,16 +1,12 @@
-from dataclasses import replace
-
 from cli import describe
 from game import State
 
 
 def test_active_player_0():
-    state = State.init()
-    state = replace(state, active_player=0)
+    state = State(player_count=2, active_player=0)
     assert describe(state) == "The active player is 0\n"
 
 
 def test_active_player_1():
-    state = State.init()
-    state = replace(state, active_player=1)
+    state = State(player_count=2, active_player=1)
     assert describe(state) == "The active player is 1\n"
