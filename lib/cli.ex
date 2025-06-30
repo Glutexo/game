ed2c2f869fd9f0d11be48cb14bd1b24ad9503000
Glutexo <glutexo@icloud.com>
2025-07-01 00:09:11 +0200

    Create a rudimentary game
    Player 1 always wins.

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,6 +1,13 @@
 defmodule Game.CLI do
   def main(_) do
-    Game.hello
-    |> IO.puts()
+    turn()
+    IO.puts("Player 1 wins!")
+  end
+
+  def turn() do
+    case IO.gets("Player 1’s turn.\nAction? ") do
+      :eof -> IO.puts("")
+      _ -> turn()
+    end
   end
 end
