73daf256dd91fcb11385d77ce6024d75898fe833
Glutexo <glutexo@icloud.com>
2025-06-30 23:57:42 +0200

    Create a CLI escript

---
+++ lib/cli.ex
@@ -0,0 +1,6 @@
+defmodule Game.CLI do
+  def main(_) do
+    Game.hello
+    |> IO.puts()
+  end
+end
--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Game.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      escript: [main_module: Game.CLI]
     ]
   end
 
